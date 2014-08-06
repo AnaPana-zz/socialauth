78755ad85e83ef56d54e9884afdc522f392d7a22
esyn <ia.filatov@gmail.com>
2014-08-06 22:36:26 +0300

    Configure settings for PostgreSQL

--- itsup/settings.py
+++ itsup/settings.py
@@ -57,8 +57,12 @@ WSGI_APPLICATION = 'itsup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'G4skU1^!QXJz#UQK',
+        'HOST': 'itsup_pgsql',
+        'PORT': 5432,
     }
 }
 
