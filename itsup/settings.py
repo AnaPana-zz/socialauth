ee6ebaff0f535cf4c05e2ed1d15a896809d36b31
AnaPana <Ana7Pana@gmail.com>
2015-01-11 19:18:31 +0200

    Add social-auth settings. Create custom user model and manager.

--- itsup/settings.py
+++ itsup/settings.py
@@ -88,6 +88,77 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = '/data/web/static'
 
+AUTHENTICATION_BACKENDS = (
+    'social.backends.facebook.FacebookOAuth2',
+    'social.backends.google.GoogleOAuth2',
+    'social.backends.linkedin.LinkedinOAuth',
+    'social.backends.dropbox.DropboxOAuth',
+    'social.backends.github.GithubOAuth2',
+    'social.backends.vk.VKOAuth2',
+    'social.backends.stackoverflow.StackoverflowOAuth2',
+    'social.backends.email.EmailAuth',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+AUTH_USER_MODEL = 'user_auth_app.CustomUser'
+
+LOGIN_URL          = '/auth_app/login/'
+LOGIN_REDIRECT_URL = '/auth_app/logged-in/'
+LOGIN_ERROR_URL    = '/auth_app/login-error/'
+SOCIAL_AUTH_DISCONNECT_REDIRECT_URL = '/auth_app/logout/'
+SOCIAL_AUTH_COMPLETE_URL_NAME  = 'socialauth_complete'
+
+# These environment variables are different for production and local servers
+# I've created two applications on all of the resources for testing on localhost and on Openshift
+SOCIAL_AUTH_FACEBOOK_KEY = os.environ['SOCIAL_AUTH_FACEBOOK_KEY']
+SOCIAL_AUTH_FACEBOOK_SECRET = os.environ['SOCIAL_AUTH_FACEBOOK_SECRET']
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']  # facebook doesn't provide email address by default
+# Note, that some facebook users can have unconfirmed email. In this case API wouldn't return it.
+
+SOCIAL_AUTH_LINKEDIN_KEY = os.environ['SOCIAL_AUTH_LINKEDIN_KEY']
+SOCIAL_AUTH_LINKEDIN_SECRET = os.environ['SOCIAL_AUTH_LINKEDIN_SECRET']
+SOCIAL_AUTH_LINKEDIN_SCOPE = ['r_basicprofile', 'r_emailaddress']
+SOCIAL_AUTH_LINKEDIN_FIELD_SELECTORS = ['email-address']  # linkedin doesn't provide email address by default
+SOCIAL_AUTH_LINKEDIN_EXTRA_DATA = [('id', 'id'),
+                                   ('firstName', 'first_name'),
+                                   ('lastName', 'last_name'),
+                                   ('emailAddress', 'email_address')]
+
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ['SOCIAL_AUTH_GOOGLE_OAUTH2_KEY']
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ['SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET']
+
+SOCIAL_AUTH_GITHUB_KEY = os.environ['SOCIAL_AUTH_GITHUB_KEY']
+SOCIAL_AUTH_GITHUB_SECRET = os.environ['SOCIAL_AUTH_GITHUB_SECRET']
+SOCIAL_AUTH_GITHUB_SCOPE = ['user:email']
+
+SOCIAL_AUTH_DROPBOX_KEY = os.environ['SOCIAL_AUTH_DROPBOX_KEY']
+SOCIAL_AUTH_DROPBOX_SECRET = os.environ['SOCIAL_AUTH_DROPBOX_SECRET']
+
+SOCIAL_AUTH_VK_OAUTH2_KEY = os.environ['SOCIAL_AUTH_VK_OAUTH2_KEY']
+SOCIAL_AUTH_VK_OAUTH2_SECRET = os.environ['SOCIAL_AUTH_VK_OAUTH2_SECRET']
+SOCIAL_AUTH_VK_OAUTH2_SCOPE = ['email']
+
+SOCIAL_AUTH_STACKOVERFLOW_KEY = os.environ['SOCIAL_AUTH_STACKOVERFLOW_KEY']
+SOCIAL_AUTH_STACKOVERFLOW_SECRET = os.environ['SOCIAL_AUTH_STACKOVERFLOW_SECRET']
+
+
+SOCIAL_AUTH_PIPELINE = (
+    'social.pipeline.social_auth.social_details',
+    'social.pipeline.social_auth.social_uid',
+    'social.pipeline.social_auth.auth_allowed',
+    'social.pipeline.social_auth.social_user',
+    'social.pipeline.user.get_username',
+    'social.pipeline.social_auth.associate_by_email',
+    'social.pipeline.mail.mail_validation',
+    'social.pipeline.user.create_user',
+    'social.pipeline.social_auth.associate_user',
+    'social.pipeline.social_auth.load_extra_data',
+    'social.pipeline.user.user_details'
+)
+
+SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
+
 
 # Logging
 
--- user_auth_app/models.py
+++ user_auth_app/models.py
@@ -1,3 +1,51 @@
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class UserDetailsValidationException(Exception):
+    """
+    Exceptions for not proper user's information
+    """
+    pass
+
+
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, *args, **kwargs):
+        super(CustomUserManager, self).create_user(*args, **kwargs)
+
+    def create_superuser(self, *args, **kwargs):
+        super(CustomUserManager, self).create_superuser(*args, **kwargs)
+
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=40, unique=True)
+
+    first_name = models.CharField(max_length=40, blank=True)
+    last_name = models.CharField(max_length=40, blank=True)
+    connected_providers = models.CharField(max_length=140, blank=True, default='')
+
+    is_superuser = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'username'
+    # REQUIRED_FIELDS = ['username']
+
+    def __unicode__(self):
+        return self.email
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return ' '.join([self.first_name, self.last_name])
+
+    def get_short_name(self):
+        return self.first_name
+
