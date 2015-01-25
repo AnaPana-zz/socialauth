dfcb2a570003897096e4c155af7879c75d6f7080
AnaPana <Ana7Pana@gmail.com>
2015-01-25 18:21:55 +0200

    Minimize custom user"s form and model. Remove first name and last name fields.

--- user_auth_app/forms.py
+++ user_auth_app/forms.py
@@ -1,34 +1,36 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
+from django.core import validators
+
 
 class CustomUserForm(forms.ModelForm):
 
-    error_messages = {
-        'duplicate_username': "A user with that username already exists.",
-        'password_mismatch': "The two password fields didn't match.",
-    }
     password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
     password2 = forms.CharField(label="Password confirmation", widget=forms.PasswordInput)
 
     class Meta:
         model = CustomUser
-        fields = ('first_name', 'last_name', 'email', 'password1', 'password2',)
+        fields = ('username', 'email', 'password1', 'password2',)
 
     def clean_password2(self):
         password1 = self.cleaned_data.get("password1")
         password2 = self.cleaned_data.get("password2")
         if password1 and password2 and password1 != password2:
-            raise forms.ValidationError(
-                self.error_messages['password_mismatch'],
-                code='password_mismatch',
-            )
+            raise forms.ValidationError("Passwords are not equal.")
         return password2
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        try:
+            CustomUser.objects.get(username=username)
+        except CustomUser.DoesNotExist:
+            return username
+        raise validators.ValidationError("Custom user with username \"%s\" already exists." % username)
 
     def save(self, commit=True):
         user = super(CustomUserForm, self).save(commit=False)
-        user.username = self.cleaned_data["email"]
         user.set_password(self.cleaned_data["password1"])
         if commit:
+            user.is_active = False
             user.save()
         return user
--- user_auth_app/models.py
+++ user_auth_app/models.py
@@ -38,10 +38,8 @@ class CustomUserManager(BaseUserManager):
 class CustomUser(AbstractBaseUser):
 
     email = models.EmailField(unique=True)
-    username = models.CharField(max_length=40, unique=True)
+    username = models.CharField(max_length=40, unique=True) # public display name
 
-    first_name = models.CharField(max_length=40, blank=True)
-    last_name = models.CharField(max_length=40, blank=True)
     connected_providers = models.CharField(max_length=140, blank=True, default='')
 
     is_superuser = models.BooleanField(default=False)
@@ -59,10 +57,4 @@ class CustomUser(AbstractBaseUser):
 
     def __str__(self):
         return self.email
-
-    def get_full_name(self):
-        return ' '.join([self.first_name, self.last_name])
-
-    def get_short_name(self):
-        return self.first_name
 
